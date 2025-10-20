c30ace0f08572638bc7ff7073e70f67620c00eb9
syw1236 <syw1236@naver.com>
2025-10-20 19:00:01 +0900

    feat(app): 문자열 덧셈 계산기 실행 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
 import { MESSAGE } from "./constants.js";
-
+import { strParser } from './parser.js';
+import { sum } from "./calculator.js";
 class App {
   async run() {
     Console.print(MESSAGE.explan);
     const input = await Console.readLineAsync(MESSAGE.explan);
-    Console.print(MESSAGE.result);
+    const numbers = strParser(input);
+    const result = sum(numbers);
+    Console.print(MESSAGE.result + result);
   }
 }
 

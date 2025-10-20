3b30929b181de20b8502bace4ebc5196f04c652b
syw1236 <syw1236@naver.com>
2025-10-20 18:43:28 +0900

    feat(app): 문자열 입출력 로직과 MESSAGE, PATTERN 상수 정의

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MESSAGE } from "./constants.js";
+
 class App {
-  async run() {}
+  async run() {
+    Console.print(MESSAGE.explan);
+    const input = await Console.readLineAsync(MESSAGE.explan);
+    Console.print(MESSAGE.result);
+  }
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const MESSAGE = {
+    explan: "덧셈할 문자열을 입력해 주세요.",
+    result: "결과 : ",
+    
+};
+
+export const PATTERN = {
+    custom: /^\/\/(\[.*\]|.)\n(.*)$/,
+    basic: /[,:]/,
+}
+
+export const DELIMITER = {
+    basic: [",", ":"],
+}
